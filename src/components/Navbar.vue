b6d1a3ad802158940133b5f141b3c936ded5a14f
Aaron Goodrum <ag505181@gmail.com>
2017-12-17 15:10:03 -0800

    building view for vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav>
+    <div class="nav-wrapper green lighten-3">
+      <div class="container">
+        <router-link to="/" class="brand-logo">
+            Employee Manager
+        </router-link>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{
+  apiKey: "AIzaSyCj8xAQYIZVudAidotYCHUj4u5WrEg3Z6g",
+  authDomain: "cinema-movies.firebaseapp.com",
+  databaseURL: "https://cinema-movies.firebaseio.com",
+  projectId: "cinema-movies",
+  storageBucket: "cinema-movies.appspot.com",
+  messagingSenderId: "874026795961"
+
+}
